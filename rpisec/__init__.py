91fb010d1673332c8d62041b94e2c28b8b3726d7
Elwyn Malethan <emalethan@Elwyns-iMac.local>
2020-08-11 22:24:17 +0100

    stopping packet sniffing

--- rpisec/__init__.py
+++ rpisec/__init__.py
@@ -3,5 +3,6 @@
 from .rpis_security import RpisSecurity
 from .rpis_camera import RpisCamera
 from .rpis_state import RpisState
-from .threads import process_photos, capture_packets, monitor_alarm_state, telegram_bot
+# from .threads import process_photos, capture_packets, monitor_alarm_state, telegram_bot
+from .threads import process_photos, monitor_alarm_state, telegram_bot
 from .exit_clean import exit_clean, exit_error, exception_handler
--- rpisec/threads/__init__.py
+++ rpisec/threads/__init__.py
@@ -2,5 +2,5 @@
 
 from .telegram_bot import telegram_bot
 from .monitor_alarm_state import monitor_alarm_state
-from .capture_packets import capture_packets
+# from .capture_packets import capture_packets
 from .process_photos import process_photos
